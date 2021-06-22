00a82c032653a2ce794bb51a603c9a89d0fd29be
José <josehenriqueiel@gmail.com>
2021-06-22 10:57:39 -0300

    formulario pronto/ sem responsividade

--- script.js
+++ script.js
@@ -4,11 +4,14 @@ const prevBtnSec = document.querySelector(".prev-1");
 const nextBtnSec = document.querySelector(".next-1");
 const prevBtnThird = document.querySelector(".prev-2");
 const nextBtnThird = document.querySelector(".next-2");
+const nextBtnFourth = document.querySelector(".next-3");
+const prevBtnFift = document.querySelector(".prev-4");
 const prevBtnFourth = document.querySelector(".prev-3");
 const submitBtn = document.querySelector(".submit");
 const progressText = [...document.querySelectorAll(".step p")];
 const progressCheck = [...document.querySelectorAll(".step .check")];
 const bullet = [...document.querySelectorAll(".step .bullet")];
+
 let max = 4;
 let current = 1;
 
@@ -33,6 +36,15 @@ nextBtnThird.addEventListener("click", function(){
   progressText[current - 1].classList.add("active");
   current += 1;
 });
+
+nextBtnFourth.addEventListener("click", function(){
+  slidePage.style.marginLeft = "-100%";
+  bullet[current - 1].classList.add("active");
+  progressCheck[current - 1].classList.add("active");
+  progressText[current - 1].classList.add("active");
+  current += 1;
+});
+
 submitBtn.addEventListener("click", function(){
   bullet[current - 1].classList.add("active");
   progressCheck[current - 1].classList.add("active");
@@ -60,6 +72,14 @@ prevBtnThird.addEventListener("click", function(){
 });
 prevBtnFourth.addEventListener("click", function(){
   slidePage.style.marginLeft = "-50%";
+  bullet[current - 2].classList.remove("active");
+  progressCheck[current - 2].classList.remove("active");
+  progressText[current - 2].classList.remove("active");
+  current -= 1;
+});
+
+prevBtnFift.addEventListener("click", function(){
+  slidePage.style.marginLeft = "-75%";
   bullet[current - 2].classList.remove("active");
   progressCheck[current - 2].classList.remove("active");
   progressText[current - 2].classList.remove("active");
