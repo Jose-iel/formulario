23febed102d73b7073ee1e719aecf5cbee66de76
José <josehenriqueiel@gmail.com>
2021-06-28 15:53:22 -0300

    Formulario versão 1.0

--- dados.js
+++ dados.js
@@ -1,112 +1,136 @@
-const data = {
-    name: '',
-    email: '',
-    office: '',
-    workers: '',
-    business: '',
-    s2option1: '',
-    s2option2: '',
-    s2option3: '',
-    s2option4: '',
-    s2option5: '',
-    s2option6: '',
-    s3option1: '',
-    s3option2: '',
-    s3option3: '',
-    s3option4: '',
-    s3option5: '',
-    s3option6: '',
-    s3option7: '',
-    analytics: '',
-    history: '',
-    message: '',
-}
+let data = {};
 
 function salvarDados(){
     //SECTION-01
-    data.name = document.getElementById('name').value;
-    data.email = document.getElementById('email').value;
-    data.office = document.getElementById('office').value;
-    data.workers = document.getElementById('workers').value;
-    data.business = document.getElementById('business').value;
+    data.select = document.getElementById('select').value;
+
+    if(document.getElementById("name").value.length > 3){
+        data.name = document.getElementById('name').value;
+    }
+
+    if(document.getElementById("email").value.length > 3){
+        data.email = document.getElementById('email').value;
+    }
+
+    if(document.getElementById("office").value.length > 3){
+        data.office = document.getElementById('office').value;
+    }
+
+
+    const option01 = document.getElementById('ate50');
+    const option02 = document.getElementById('ate300');
+    const option03 = document.getElementById('ate1000');
+    const option04 = document.getElementById('acima1000');
+
+    if(option01.checked){
+        const dados = option01.value;
+        data.ate50 = dados
+    }
+    
+    if(option02.checked){
+        const dados = option02.value;
+        data.ate300 = dados
+    }
+
+    if(option03.checked){
+        const dados = option03.value;
+        data.ate1000 = dados
+    }
+
+    if(option04.checked){
+        const dados = option04.value;
+        data.acima1000 = dados
+    }
 
     //SECTION-02
-    const s2option1 = document.getElementById('s2option1');
-    const s2option2 = document.getElementById('s2option2');
-    const s2option3 = document.getElementById('s2option3');
-    const s2option4 = document.getElementById('s2option4');
-    const s2option5 = document.getElementById('s2option5');
-    const s2option6 = document.getElementById('s2option6');
+    const dataLake = document.getElementById('s2option1');
+    const dataWhere = document.getElementById('s2option2');
+    const governanca = document.getElementById('s2option3');
+    const machieLearning = document.getElementById('s2option4');
+    const selfService = document.getElementById('s2option5');
+    const realTimeAnalytics = document.getElementById('s2option6');
 
-    if (s2option1.checked){
-        const dados = s2option1.value
-        data.s2option1 = dados
+    if (dataLake.checked){
+        const dados = dataLake.value
+        data.dataLake = dados
     }
-    if (s2option2.checked){
-        const dados = s2option2.value
-        data.s2option2 = dados
+    if (dataWhere.checked){
+        const dados = dataWhere.value
+        data.dataWhere = dados
     }
-    if (s2option3.checked){
-        const dados = s2option3.value
-        data.s2option3 = dados
+    if (governanca.checked){
+        const dados = governanca.value
+        data.governanca = dados
     }
-    if (s2option4.checked){
-        const dados = s2option4.value
-        data.s2option4 = dados
+    if (machieLearning.checked){
+        const dados = machieLearning.value
+        data.machieLearning = dados
     }
-    if (s2option5.checked){
-        const dados = s2option5.value
-        data.s2option5 = dados
+    if (selfService.checked){
+        const dados = selfService.value
+        data.selfService = dados
     }
-    if (s2option6.checked){
-        const dados = s2option6.value
-        data.s2option6 = dados
+    if (realTimeAnalytics.checked){
+        const dados = realTimeAnalytics.value
+        data.realTimeAnalytics = dados
     }
     
     //SECTION-03
-    const s3option1 = document.getElementById('s3option1');
-    const s3option2 = document.getElementById('s3option2');
-    const s3option3 = document.getElementById('s3option3');
-    const s3option4 = document.getElementById('s3option4');
-    const s3option5 = document.getElementById('s3option5');
-    const s3option6 = document.getElementById('s3option6');
-    const s3option7 = document.getElementById('s3option7');
+    const onPremises = document.getElementById('s3option1');
+    const nuvem = document.getElementById('s3option2');
+    const aws = document.getElementById('s3option3');
+    const azure = document.getElementById('s3option4');
+    const gcp = document.getElementById('s3option5');
+    const outros = document.getElementById('s3option6');
+    const hibrido = document.getElementById('s3option7');
 
-    if (s3option1.checked){
-        const dados = s3option1.value
-        data.s3option1 = dados
+    if (onPremises.checked){
+        const dados = onPremises.value
+        data.onPremises = dados
     }
-    if (s3option2.checked){
-        const dados = s3option2.value
-        data.s3option2 = dados
+    if (nuvem.checked){
+        const dados = nuvem.value
+        data.nuvem = dados
     }
-    if (s3option3.checked){
-        const dados = s3option3.value
-        data.s3option3 = dados
+    if (aws.checked){
+        const dados = aws.value
+        data.aws = dados
     }
-    if (s3option4.checked){
-        const dados = s3option4.value
-        data.s3option4 = dados
+    if (azure.checked){
+        const dados = azure.value
+        data.azure = dados
     }
-    if (s3option5.checked){
-        const dados = s3option5.value
-        data.s3option5 = dados
+    if (gcp.checked){
+        const dados = gcp.value
+        data.gcp = dados
     }
-    if (s3option6.checked){
-        const dados = s3option6.value
-        data.s3option6 = dados
+    if (outros.checked){
+        const dados = outros.value
+        data.outros = dados
     }
-    if (s3option7.checked){
-        const dados = s3option7.value
-        data.s3option7 = dados
+    if (hibrido.checked){
+        const dados = hibrido.value
+        data.hibrido = dados
     }
 
     //SECTION-04
-    data.analytics = document.getElementById('analytics').value;
-    data.history = document.getElementById('history').value;
+    if(document.getElementById("analytics").value.length > 3){
+        data.analytics = document.getElementById('analytics').value;
+    }
+
+    if(document.getElementById("history").value.length > 3){
+        data.history = document.getElementById('history').value;
+    }
 
     //SECTION-05
-    data.message = document.getElementById('message').value;
-
+    if(document.getElementById("message").value.length > 3){
+        data.message = document.getElementById('message').value;
+    }
+    
     console.log(data);
+    //let arqJSON = JSON.stringify(data);
+}
+
+function imprimir(){
+    console.log('Aqui vai os dados para imprimir');
 }
--- script.js
+++ script.js
@@ -12,6 +12,7 @@ const progressText = [...document.querySelectorAll(".step p")];
 const progressCheck = [...document.querySelectorAll(".step .check")];
 const bullet = [...document.querySelectorAll(".step .bullet")];
 
+
 let max = 4;
 let current = 1;
 
@@ -50,9 +51,9 @@ submitBtn.addEventListener("click", function(){
   progressCheck[current - 1].classList.add("active");
   progressText[current - 1].classList.add("active");
   current += 1;
-  // setTimeout(function(){
-  //   location.reload();
-  // },800);
+  setTimeout(function(){
+    window.location.replace("download.html");
+  },500);
 });
 
 prevBtnSec.addEventListener("click", function(){
